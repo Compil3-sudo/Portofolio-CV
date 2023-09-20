b3a453b50a306280abb94676da0a6a9d78d927ce
tincurar <tincurares13@gmail.com>
2023-09-20 14:06:54 +0300

    changed background, transition duration btn hover

--- portofolio-cv/src/components/button/Button.jsx
+++ portofolio-cv/src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Button = ({ customClass, onClick, children }) => {
   let btnClasses =
-    "mx-2 h-min bg-transparent hover:bg-[#10b981] text-[#06b6d4] font-semibold hover:text-white border border-[#06b6d4] hover:border-transparent focus:outline-none rounded";
+    "mx-2 h-min bg-transparent hover:bg-[#10b981] text-[#06b6d4] font-semibold hover:text-white border border-[#06b6d4] hover:border-transparent focus:outline-none rounded transition duration-300";
 
   if (customClass && customClass !== "") {
     btnClasses = customClass;
